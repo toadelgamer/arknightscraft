76bc0098855aada2344309ae0189199976803a4f
toadelgamer <toadelgamer123@gmail.com>
2025-05-15 08:24:24 -0400

    Uploaded mod files

---
+++ src/main/java/net/akcraft/ArknightsCraft.java
@@ -0,0 +1,18 @@
+package net.akcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArknightsCraft implements ModInitializer {
+	public static final String MOD_ID = "arknightscraft";
+
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/akcraft/akcraftClient.java
@@ -0,0 +1,10 @@
+package net.akcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class akcraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
